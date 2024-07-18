69df64fc24c2f16b7bff792425e7c090e0c52aaa
Luis Henrique <krzx3uchz@gmail.com>
2024-07-17 21:18:09 -0300

    modified:   data-analytic2.py

--- data-analytic2.py
+++ data-analytic2.py
@@ -106,39 +106,6 @@ with tab1:
 
     st.pyplot(fig)
 
-    tarefas_por_hora = df_desempenho.groupby(['Usuário', 'Hora']).size().reset_index(name='Qtde Tarefas')
-    tarefas_por_hora['Hora'] = tarefas_por_hora['Hora'].apply(lambda x: x.strftime('%H:%M'))
-    tarefas_por_hora = tarefas_por_hora.sort_values(by=['Usuário', 'Hora'])
-    tarefas_por_hora['Ordenacao'] = tarefas_por_hora['Hora'].apply(lambda x: (pd.to_datetime(str(x), format='%H:%M') + pd.DateOffset(hours=5)).time())
-    tarefas_por_hora = tarefas_por_hora.sort_values(by=['Usuário', 'Ordenacao'])
-    tarefas_por_hora = tarefas_por_hora.drop('Ordenacao', axis=1)
-    tarefas_pivot = tarefas_por_hora.pivot_table(index='Usuário', columns='Hora', values='Qtde Tarefas', fill_value=0)
-    tarefas_pivot = tarefas_pivot.reindex(columns=sorted(tarefas_pivot.columns, key=lambda x: (pd.to_datetime(str(x), format='%H:%M') + pd.DateOffset(hours=5)).time()))
-    sum_values = tarefas_pivot.sum()
-    tarefas_pivot.loc['Total P/ Hora'] = sum_values
-    tarefas_pivot['Total'] = tarefas_pivot.sum(axis=1)
-    tarefas_pivot['Total'] = tarefas_pivot['Total'].astype(int)
-
-    st.subheader('Tarefas por Hora')
-    st.write(tarefas_pivot)
-
-    tarefas_pivot = tarefas_pivot.drop(columns='Total')
-    total_hora_data = tarefas_pivot.loc['Total P/ Hora']
-
-    plt.figure(figsize=(12, 6))
-    plt.plot(total_hora_data.index, total_hora_data.values, marker='o', linestyle='-', color='black', label='Total de Tarefas')
-
-    for i, (hora, total) in enumerate(total_hora_data.items()):
-        plt.annotate(f'{int(total)}', (hora, total), textcoords="offset points", xytext=(0, 10), ha='center')
-
-    plt.title('Total de Tarefas por Hora')
-    plt.xlabel('Hora')
-    plt.ylabel('Quantidade Total de Tarefas')
-    plt.xticks(rotation=45)
-    plt.legend(loc='upper left')
-    plt.grid(True)
-    plt.tight_layout()
-    st.pyplot(plt)
 
 with tab2:
     # Título da Aplicação
