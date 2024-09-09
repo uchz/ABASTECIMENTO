42b3ccb01f5a25473ad8e38bde970f94d327d313
uchz <krzx3uchz@gmail.com>
2024-09-09 15:51:43 -0300

    modified:   data-analytic2.py 	modified:   analise_abastecimento.py

--- data-analytic2.py
+++ data-analytic2.py
@@ -12,6 +12,7 @@ from pandas.plotting import table
 # Título da Aplicação
 st.title('Acompanhamento Operação Noturno')
 
+st.markdown('ATUALIZAÇÕES AS: **21:15** / 22:15 / 23:15 / 00:15 / 01:15 / 02:15 / 03:15 / 04:15')
 # Criação das abas
 tab5, tab1, tab2, tab3, tab4,  = st.tabs(['Apanhas',"Abastecimento", "Separação Volumoso", "Varejo", "Confinado"])
 
@@ -72,17 +73,17 @@ with tab1:
 
     contagem_tipos = corretivo_preventivo.groupby('Usuário')['Tipo '].count().sort_values(ascending=False)
 
-    fig, ax = plt.subplots(figsize=(10, 6), dpi=600)
-    bars = contagem_tipos.plot(kind='bar', color='red', ax=ax)
+    #fig, ax = plt.subplots(figsize=(10, 6), dpi=600)
+    #bars = contagem_tipos.plot(kind='bar', color='red', ax=ax)
 
-    for bar in bars.patches:
-        ax.text(bar.get_x() + bar.get_width() / 2, bar.get_height() + 0.3, bar.get_height(), 
-                ha='center', va='bottom', fontsize=8)
-    plt.title('Total de Corretivos + Preventivos por Empilhador')
-    plt.xlabel('Empilhador')
-    plt.ylabel('Total de Abastecimentos')
-    plt.xticks(rotation=0)
-    st.pyplot(plt)
+    #for bar in bars.patches:
+    #     ax.text(bar.get_x() + bar.get_width() / 2, bar.get_height() + 0.3, bar.get_height(), 
+    #             ha='center', va='bottom', fontsize=8)
+    # plt.title('Total de Corretivos + Preventivos por Empilhador')
+    # plt.xlabel('Empilhador')
+    # plt.ylabel('Total de Abastecimentos')
+    # plt.xticks(rotation=0)
+    # st.pyplot(plt)
 
     contagem_tipos = df_desempenho.groupby(['Usuário', 'Tipo ']).size().unstack(fill_value=0)
     contagem_tipos['Total'] = contagem_tipos.sum(axis=1)
@@ -283,6 +284,40 @@ with tab2:
     st.write("Tarefas por Hora:")
     st.dataframe(tarefas_pivot_styled)
 
+    tarefas_por_hora = volumoso.groupby(['Usuário', 'Hora']).size().reset_index(name='Qtde Tarefas')
+    tarefas_por_hora['Hora'] = tarefas_por_hora['Hora'].apply(lambda x: x.strftime('%H:%M'))
+    tarefas_por_hora = tarefas_por_hora.sort_values(by=['Usuário', 'Hora'])
+    tarefas_por_hora['Ordenacao'] = tarefas_por_hora['Hora'].apply(lambda x: (pd.to_datetime(str(x), format='%H:%M') + pd.DateOffset(hours=5)).time())
+    tarefas_por_hora = tarefas_por_hora.sort_values(by=['Usuário', 'Ordenacao'])
+    tarefas_por_hora = tarefas_por_hora.drop('Ordenacao', axis=1)
+    tarefas_pivot = tarefas_por_hora.pivot_table(index='Usuário', columns='Hora', values='Qtde Tarefas', fill_value=0)
+    tarefas_pivot = tarefas_pivot.reindex(columns=sorted(tarefas_pivot.columns, key=lambda x: (pd.to_datetime(str(x), format='%H:%M') + pd.DateOffset(hours=5)).time()))
+    sum_values = tarefas_pivot.sum()
+    tarefas_pivot.loc['Total P/ Hora'] = sum_values
+    tarefas_pivot['Total'] = tarefas_pivot.sum(axis=1)
+    tarefas_pivot['Total'] = tarefas_pivot['Total'].astype(int)
+
+    st.subheader('Tarefas por Hora')
+    st.write(tarefas_pivot)
+
+    tarefas_pivot = tarefas_pivot.drop(columns='Total')
+    total_hora_data = tarefas_pivot.loc['Total P/ Hora']
+
+    plt.figure(figsize=(12, 6))
+    plt.plot(total_hora_data.index, total_hora_data.values, marker='o', linestyle='-', color='black', label='Total de Tarefas')
+
+    for i, (hora, total) in enumerate(total_hora_data.items()):
+        plt.annotate(f'{int(total)}', (hora, total), textcoords="offset points", xytext=(0, 10), ha='center')
+
+    plt.title('Total de Tarefas por Hora')
+    plt.xlabel('Hora')
+    plt.ylabel('Quantidade Total de Tarefas')
+    plt.xticks(rotation=45)
+    plt.legend(loc='upper left')
+    plt.grid(True)
+    plt.tight_layout()
+    st.pyplot(plt)
+
 with tab3:
 
     st.subheader("Acompanhamento Separação Varejo")
@@ -360,18 +395,18 @@ with tab3:
     tarefas_pivot = tarefas_pivot.reindex(columns=sorted(tarefas_pivot.columns, key=lambda x: (pd.to_datetime(str(x), format='%H:%M') + pd.DateOffset(hours=5)).time()))
 
     # Calculando a média de cada coluna de horas
-    mean_values = tarefas_pivot.mean()
+    #mean_values = tarefas_pivot.mean()
 
-    mean_values = mean_values.round(0).astype(int)
+    #mean_values = mean_values.round(0).astype(int)
 
     # Adicionando as médias como uma nova linha ao DataFrame tarefas_pivot
-    tarefas_pivot.loc['Média Hora'] = mean_values
+    #tarefas_pivot.loc['Média Hora'] = mean_values
 
-    mean_total  = mean_values.mean().mean()
+    #mean_total  = mean_values.mean().mean()
 
-    sum_values = tarefas_pivot.sum()
+    #sum_values = tarefas_pivot.sum()
 
-    sum_values = tarefas_pivot.drop('Média Hora').sum()
+    #sum_values = tarefas_pivot.drop('Média Hora').sum()
 
 
     tarefas_pivot.loc['Total P/ Hora'] = sum_values
@@ -385,9 +420,9 @@ with tab3:
 
 
     tarefas_pivot['Total'] = tarefas_pivot.sum(axis=1)
-    tarefas_pivot['Total'] = tarefas_pivot['Total'].drop('Média Hora')
+    #tarefas_pivot['Total'] = tarefas_pivot['Total'].drop('Média Hora')
 
-    tarefas_pivot['Total'].fillna(mean_total, inplace=True)
+    #tarefas_pivot['Total'].fillna(mean_total, inplace=True)
 
     tarefas_pivot['Total'] = tarefas_pivot['Total'].astype(int)
 
@@ -398,31 +433,40 @@ with tab3:
        # Exibir a tabela dinâmica com estilos de cor
     st.write(tarefas_pivot_styled)
 
-    st.write('Média por Hora')
-    media_hora_data = tarefas_pivot.loc['Média Hora'][:-1]
+    tarefas_por_hora = varejo.groupby(['Usuário', 'Hora']).size().reset_index(name='Qtde Tarefas')
+    tarefas_por_hora['Hora'] = tarefas_por_hora['Hora'].apply(lambda x: x.strftime('%H:%M'))
+    tarefas_por_hora = tarefas_por_hora.sort_values(by=['Usuário', 'Hora'])
+    tarefas_por_hora['Ordenacao'] = tarefas_por_hora['Hora'].apply(lambda x: (pd.to_datetime(str(x), format='%H:%M') + pd.DateOffset(hours=5)).time())
+    tarefas_por_hora = tarefas_por_hora.sort_values(by=['Usuário', 'Ordenacao'])
+    tarefas_por_hora = tarefas_por_hora.drop('Ordenacao', axis=1)
+    tarefas_pivot = tarefas_por_hora.pivot_table(index='Usuário', columns='Hora', values='Qtde Tarefas', fill_value=0)
+    tarefas_pivot = tarefas_pivot.reindex(columns=sorted(tarefas_pivot.columns, key=lambda x: (pd.to_datetime(str(x), format='%H:%M') + pd.DateOffset(hours=5)).time()))
+    sum_values = tarefas_pivot.sum()
+    tarefas_pivot.loc['Total P/ Hora'] = sum_values
+    tarefas_pivot['Total'] = tarefas_pivot.sum(axis=1)
+    tarefas_pivot['Total'] = tarefas_pivot['Total'].astype(int)
 
-    # Criando o gráfico de linha com Matplotlib
-    
-    fig, ax = plt.subplots(figsize=(12,6))
+    st.subheader('Tarefas por Hora')
 
-    # Adicionando a série de dados da média de tarefas
-    ax.plot(media_hora_data.index, media_hora_data.values, marker='o', label='Média de Tarefas', color='black')
+    st.write(tarefas_pivot)
 
-    # Adicionando a linha de meta
-    meta_value = 76  # Valor da linha de meta
-    ax.axhline(y=meta_value, color='red', linestyle='--', label='Linha de Meta')
+    tarefas_pivot = tarefas_pivot.drop(columns='Total')
+    total_hora_data = tarefas_pivot.loc['Total P/ Hora']
 
-    # Configurando layout do gráfico
-    ax.set_title('Média de Tarefas por Hora')
-    ax.set_xlabel('Hora')
-    ax.set_ylabel('Quantidade Média de Tarefas')
+    plt.figure(figsize=(12, 6))
+    plt.plot(total_hora_data.index, total_hora_data.values, marker='o', linestyle='-', color='black', label='Total de Tarefas')
+
+    for i, (hora, total) in enumerate(total_hora_data.items()):
+        plt.annotate(f'{int(total)}', (hora, total), textcoords="offset points", xytext=(0, 10), ha='center')
+
+    plt.title('Total de Tarefas por Hora')
+    plt.xlabel('Hora')
+    plt.ylabel('Quantidade Total de Tarefas')
     plt.xticks(rotation=45)
+    plt.legend(loc='upper left')
     plt.grid(True)
-    ax.legend(loc='upper right')
-
-    for i, value in enumerate(media_hora_data.values):
-
-        ax.annotate(f'{value}', (media_hora_data.index[i], value), textcoords="offset points", xytext=(0,10), ha='center')
+    plt.tight_layout()
+    st.pyplot(plt)
 
 # Configurando layout do gráfico
     df_conf = pd.read_excel('Gestao_Produtividade_detalhada_WMS_2.xlsx', header=2)
@@ -444,7 +488,7 @@ with tab3:
 
     df_conf['Hora'] = pd.to_datetime(df_conf['Hora'], format='%H').dt.time
     
-    st.pyplot(fig)
+    #st.pyplot(fig)
 
     st.header('Produtividade Conferência')
 
@@ -531,6 +575,40 @@ with tab3:
        # Exibir a tabela dinâmica com estilos de cor
     st.write(tarefas_pivot_styled)
 
+    tarefas_por_hora = conferencia.groupby(['Usuário', 'Hora']).size().reset_index(name='Qtde Tarefas')
+    tarefas_por_hora['Hora'] = tarefas_por_hora['Hora'].apply(lambda x: x.strftime('%H:%M'))
+    tarefas_por_hora = tarefas_por_hora.sort_values(by=['Usuário', 'Hora'])
+    tarefas_por_hora['Ordenacao'] = tarefas_por_hora['Hora'].apply(lambda x: (pd.to_datetime(str(x), format='%H:%M') + pd.DateOffset(hours=5)).time())
+    tarefas_por_hora = tarefas_por_hora.sort_values(by=['Usuário', 'Ordenacao'])
+    tarefas_por_hora = tarefas_por_hora.drop('Ordenacao', axis=1)
+    tarefas_pivot = tarefas_por_hora.pivot_table(index='Usuário', columns='Hora', values='Qtde Tarefas', fill_value=0)
+    tarefas_pivot = tarefas_pivot.reindex(columns=sorted(tarefas_pivot.columns, key=lambda x: (pd.to_datetime(str(x), format='%H:%M') + pd.DateOffset(hours=5)).time()))
+    sum_values = tarefas_pivot.sum()
+    tarefas_pivot.loc['Total P/ Hora'] = sum_values
+    tarefas_pivot['Total'] = tarefas_pivot.sum(axis=1)
+    tarefas_pivot['Total'] = tarefas_pivot['Total'].astype(int)
+
+    st.subheader('Tarefas por Hora')
+    st.write(tarefas_pivot)
+
+    tarefas_pivot = tarefas_pivot.drop(columns='Total')
+    total_hora_data = tarefas_pivot.loc['Total P/ Hora']
+
+    plt.figure(figsize=(12, 6))
+    plt.plot(total_hora_data.index, total_hora_data.values, marker='o', linestyle='-', color='black', label='Total de Tarefas')
+
+    for i, (hora, total) in enumerate(total_hora_data.items()):
+        plt.annotate(f'{int(total)}', (hora, total), textcoords="offset points", xytext=(0, 10), ha='center')
+
+    plt.title('Total de Tarefas por Hora')
+    plt.xlabel('Hora')
+    plt.ylabel('Quantidade Total de Tarefas')
+    plt.xticks(rotation=45)
+    plt.legend(loc='upper left')
+    plt.grid(True)
+    plt.tight_layout()
+    st.pyplot(plt)
+
 with tab4:
     area_confinado = ['SEP CONFINADO']
 
@@ -598,20 +676,30 @@ with tab5:
 
     df_feito_total = pd.DataFrame({
         'Situação': ['Feito'],
-        'Varejo': [varejo_feito['Qtd. Tarefas'].sum()],
-        'Confinado': [confinado_feito['Qtd. Tarefas'].sum()],
-        'Volumoso': [volumoso_feito['Qtd. Tarefas'].sum()]
+        'Varejo': [int(varejo_feito['Qtd. Tarefas'].sum())],
+        'Confinado': [int(confinado_feito['Qtd. Tarefas'].sum())],
+        'Volumoso': [int(volumoso_feito['Qtd. Tarefas'].sum())]
         
     })
 
     df_importados = pd.DataFrame({
         'Situação': ['Importados'],
-        'Varejo': [varejo['Qtd. Tarefas'].sum()],
-        'Confinado': [confinado['Qtd. Tarefas'].sum()],
-        'Volumoso': [volumoso['Qtd. Tarefas'].sum()]        
+        'Varejo': [int(varejo['Qtd. Tarefas'].sum())],
+        'Confinado': [int(confinado['Qtd. Tarefas'].sum())],
+        'Volumoso': [int(volumoso['Qtd. Tarefas'].sum())]        
     })
     
-    
+    percent_varejo = (df_feito_total['Varejo'].values[0] / df_importados['Varejo'].values[0]) * 100
+    percent_confinado = (df_feito_total['Confinado'].values[0] / df_importados['Confinado'].values[0]) * 100
+    percent_volumoso = (df_feito_total['Volumoso'].values[0] / df_importados['Volumoso'].values[0]) * 100
+
+    # 3. Adicionar uma linha com as porcentagens
+    df_percent = pd.DataFrame({
+        'Situação': ['Porcentagem Feito'],
+        'Varejo': [f'{percent_varejo:.2f}%'],
+        'Confinado': [f'{percent_confinado:.2f}%'],
+        'Volumoso': [f'{percent_volumoso:.2f}%']
+    })
 
 
 
@@ -619,7 +707,8 @@ with tab5:
 
 
 
+    resultado_final = pd.concat([df_feito_total, df_importados, df_percent], ignore_index=True)
 
 
-    resultado_final = pd.concat([df_feito_total, df_importados], ignore_index=True)
+
     st.write(resultado_final)
