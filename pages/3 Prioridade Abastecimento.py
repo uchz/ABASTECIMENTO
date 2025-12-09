dc2fad4b497d3ce404a1e8aab226904fd5db8cff
uchz <krzx3uchz@gmail.com>
2025-12-08 22:57:04 -0300

    alteração no prioridade abastecimento

--- pages/3 Prioridade Abastecimento.py
+++ pages/3 Prioridade Abastecimento.py
@@ -35,7 +35,10 @@ def definir_cancela(rua):
         return 3
 
 # Filtros
+df47 = df[df['DESCDESTINO'].str.contains('5.47', na=False)]
 df = df[~df['DESCDESTINO'].str.contains('CNX', na=False)]
+df = df[~df['DESCDESTINO'].str.contains('47', na=False)]
+df47 = df47[~df47['DESCDESTINO'].str.contains('5.46', na=False)]
 df = df[~df['AREA DE SEPA ENDEREçO DESTINO'].str.contains('PNC|VOLUMOSO', na=False)]
 
 # Normalização
@@ -51,13 +54,18 @@ df['Rua'] = df['DESCDESTINO'].str.extract(r'46\.0*?(\d{2})', expand=False)
 df = df.fillna(0)
 df['Cancela'] = df['Rua'].astype(int).apply(definir_cancela)
 
+df47['Rua'] = df47['DESCDESTINO'].str.extract(r'47\.0*?(\d{2})', expand=False)
+df47 = df47.fillna(0)
+df47['Cancela'] = 47
+
 # Distribui por cancela
 c1 = df.loc[df['Cancela'] == 1, 'NUTAREFA'].reset_index(drop=True)
 c2 = df.loc[df['Cancela'] == 2, 'NUTAREFA'].reset_index(drop=True)
 c3 = df.loc[df['Cancela'] == 3, 'NUTAREFA'].reset_index(drop=True)
+c47 = df47.loc[df47['Cancela'] == 47, 'NUTAREFA'].reset_index(drop=True)
 
-df_tarefas = pd.concat([c1, c2, c3], axis=1)
-df_tarefas.columns = ['Cancela 1', 'Cancela 2', 'Cancela 3']
+df_tarefas = pd.concat([c1, c2, c3, c47], axis=1)
+df_tarefas.columns = ['Cancela 1', 'Cancela 2', 'Cancela 3', 'Cancela 47']
 df_tarefas = df_tarefas.fillna("")
 
 for col in df_tarefas.columns:
@@ -71,3 +79,5 @@ for col in df_tarefas.columns:
 
 st.subheader("📊 Tarefas por Cancela")
 st.write(df_tarefas)
+
+st.write(df)
